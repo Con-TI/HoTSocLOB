0595d2a8c3aeaab4315fc641d92cc27908e0ac59
Con-TI <162825211+Con-TI@users.noreply.github.com>
2025-03-12 16:03:29 +0000

    qwerty

--- Simulator/LPs/lp.py
+++ Simulator/LPs/lp.py
@@ -12,63 +12,6 @@ from models.models import Positions, Orders, Users
 import numpy as np
 import scipy.stats as stats
 
-class BlockLP():
-    def __init__(self, username = 'whaleLP2'):
-        self.username = username
-        self.user = Users.objects.get(name=self.username)
-        self.pending_orders = None
-        self.positions = None
-        self.memory = []
-        self.market_conditions = {'midprice':None}
-
-    def update_all(self):
-        self._update_market_conditions()
-        self._update_pending()
-
-    def _clear_pending(self):
-        # Clear all pending orders
-        Orders.objects.filter(user=self.user).delete()
-
-    def _fetch_inventory_and_pending_orders(self):
-        # Fetches inventory and pending orders of the LP
-        stats = UserStats()
-        pending_orders = stats.fetch_pending_orders(self.user)
-        self.pending_orders = [{'price':order.price,'quantity':order.quantity} for order in pending_orders]
-        positions = stats.fetch_positions(self.user)
-        self.positions = [{'price':order.price,'quantity':order.quantity} for order in positions]
-
-    def _update_pending(self):
-        if self.memory:
-            midprice = np.median([mem['midprice'] for mem in self.memory])
-        else:
-            midprice = self.market_conditions['midprice']
-        # If midprice changes, update
-        if abs(midprice - self.market_conditions['midprice']) > 2:
-            self._clear_pending()
-            self._blocks_away_from_mid()
-
-    def _update_market_conditions(self):
-        # Fetches current market conditions
-        self.memory.append(self.market_conditions)
-        if len(self.memory)>10:
-            self.memory.pop(0)
-        
-        # Interacting with DB
-        u = PriceData()
-        self.market_conditions['midprice'] =  u.fetch_midprice()
-
-    def _blocks_away_from_mid(self):
-        #Places a bunch of pending orders far from midprice to fill when someone misinputs.
-        midprice = np.median([mem['midprice'] for mem in self.memory])
-        arr = np.arange(10,20)
-        bid_block_prices = midprice-arr
-        bid_block_prices = bid_block_prices[bid_block_prices>0]
-        ask_block_prices = midprice + arr
-        bid_orders = [{'price':bid,'quantity':10000} for bid in bid_block_prices]
-        ask_orders = [{'price':ask,'quantity':-10000} for ask in ask_block_prices]
-        orders = bid_orders + ask_orders
-        orders = [Orders(price=order['price'], quantity=order['quantity'], user=self.user) for order in orders]
-        Orders.objects.bulk_create(orders)
 
 class LP():
     def __init__(self, username = 'whaleLP'):
@@ -235,8 +178,7 @@ class LP():
 
     
 if __name__ == '__main__':
-    l = BlockLP()
-    l._clear_pending()
+    pass
     # for u in Users.objects.filter():
     #     u.delete()
     
